5d2e15d4eced19f94e91e1dabf51a5edbfd1f811
xiongyi <xiongyi940803@qq.com>
2017-08-15 14:09:40 +0800

    array.map and .array.forEach

---
+++ array/forEach.js
@@ -0,0 +1,6 @@
+var array = [1, 2, 3, 4, 5]
+array.forEach(function(item, index, array) {
+	console.log(item)
+	console.log(index)
+	console.log(array)
+})
---
+++ array/map.js
@@ -0,0 +1,1 @@
+// The map() method creates a new array with the results of calling a provided function on every element in this array.
