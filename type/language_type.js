6f63a1a17fbdae7b7f9c29f1bfb7451f48ab5158
xiongyi <xiongyi940803@qq.com>
2017-08-15 14:09:14 +0800

    学习js中的数据类型

---
+++ type/language_type.js
@@ -0,0 +1,13 @@
+// js 有六种主要类型（语言类型）
+// string number boolean null undefined object
+
+// 简单基本类型 string boolean number null undefined
+// 这些基本类型本身不是对象。
+
+// js中有许多特殊的对象子类型，称之为复杂基本类型
+// 函数 数组
+
+// js中还有一些对象子类型，称为内置对象
+// String Number Boolean Object Function Array Date RegExp Error
+
+// js引擎会自动把字面量转换为相应的对象，以便访问相应的属性或者方法
